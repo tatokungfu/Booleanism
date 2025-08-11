29f21bb9dce700e1d5060e01a55df9997e1b0e25
lebrancconvas <lebrancconvas@gmail.com>
2025-08-11 17:27:14 +0700

    ✨ feat: update main function.

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ function main() {
   let p = new P(true, "I love you.");
   let q = new P(false, "The sun rises at the west.");
 
-  let result = AND(p, q);
+  let result = AND(p.value, q.value);
 
   console.log(result);
 }
